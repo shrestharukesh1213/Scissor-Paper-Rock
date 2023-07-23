00a1552b4738ea15441e667c1d4e73bda8257b99
shrestharukesh1213 <125758326+shrestharukesh1213@users.noreply.github.com>
2023-07-23 20:15:32 +0545

    Added index.html and script.js

--- script.js
+++ script.js
@@ -1,78 +1,65 @@
 let choices = ["Scissor", "Paper", "Rock"];
 let playerWins = 0,
   computerWins = 0;
+const button = document.querySelectorAll("button");
+function selectedChoice() {
+  button.forEach((choice) => {
+    choice.addEventListener("click", () => choice.textContent.toLowerCase());
+  });
+}
+const playerSelection = selectedChoice();
+const computerSelection = computerChoices();
+
 function computerChoices() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() == "rock" && computerSelection == "Rock") {
+  if (playerSelection == "rock" && computerSelection == "Rock") {
     console.log("It's a draw");
-  } else if (
-    playerSelection.toLowerCase() == "paper" &&
-    computerSelection == "Paper"
-  ) {
+  } else if (playerSelection == "paper" && computerSelection == "Paper") {
     console.log("It's a draw");
-  } else if (
-    playerSelection.toLowerCase() == "scissor" &&
-    computerSelection == "Scissor"
-  ) {
+  } else if (playerSelection == "scissor" && computerSelection == "Scissor") {
     console.log("It's a draw");
-  } else if (
-    playerSelection.toLowerCase() == "paper" &&
-    computerSelection == "Rock"
-  ) {
+  } else if (playerSelection == "paper" && computerSelection == "Rock") {
     console.log("Player won");
     return 1;
-  } else if (
-    playerSelection.toLowerCase() == "paper" &&
-    computerSelection == "Scissor"
-  ) {
+  } else if (playerSelection == "paper" && computerSelection == "Scissor") {
     console.log("Computer Won");
     return 0;
-  } else if (
-    playerSelection.toLowerCase() == "scissor" &&
-    computerSelection == "Paper"
-  ) {
+  } else if (playerSelection == "scissor" && computerSelection == "Paper") {
     console.log("Player won");
     return 1;
-  } else if (
-    playerSelection.toLowerCase() == "scissor" &&
-    computerSelection == "Rock"
-  ) {
+  } else if (playerSelection == "scissor" && computerSelection == "Rock") {
     console.log("Computer won");
     return 0;
-  } else if (
-    playerSelection.toLowerCase() == "rock" &&
-    computerSelection == "Scissor"
-  ) {
+  } else if (playerSelection == "rock" && computerSelection == "Scissor") {
     console.log("Player won");
     return 1;
-  } else if (
-    playerSelection.toLowerCase() == "rock" &&
-    computerSelection == "Paper"
-  ) {
+  } else if (playerSelection == "rock" && computerSelection == "Paper") {
     console.log("Computer won");
     return 0;
   }
 }
-function game() {
-  for (i = 0; i < 5; i++) {
-    const playerSelection = prompt("Enter your move: ");
-    const computerSelection = computerChoices();
-    let val = playRound(playerSelection, computerSelection);
-    if (val == 1) {
-      playerWins++;
-    } else if (val == 0) {
-      computerWins++;
-    }
-  }
-  if (playerWins > computerWins) {
-    console.log("You Win!!!");
-  } else if (computerWins > playerWins) {
-    console.log("You Lose!");
-  } else if (computerWins == playerWins) {
-    console.log("It's a Tie!");
-  }
-}
-game();
+playRound(playerSelection, computerSelection);
+// function game() {
+// for (i = 0; i < 5; i++) {
+// let val = playRound(playerSelection, computerSelection);
+// if (val == 1) {
+//   playerWins++;
+// } else if (val == 0) {
+//   computerWins++;
+// }
+// }
+// if (playerWins > computerWins) {
+//   console.log("You Win!!!");
+// } else if (computerWins > playerWins) {
+//   console.log("You Lose!!");
+// } else if (computerWins == playerWins) {
+//   console.log("It's a Tie!");
+// }
+// }
+// game();
+// function selectedChoice() {
+//   return scissorButton.addEventListener("click", () => "paper");
+// }
