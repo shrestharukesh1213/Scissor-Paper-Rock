e37f1e03c72c49853094cdff6a94c1c269369244
shrestharukesh1213 <125758326+shrestharukesh1213@users.noreply.github.com>
2023-07-23 21:39:03 +0545

    Added code for manipulating output div to show output

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let choices = ["Scissor", "Paper", "Rock"];
 const button = document.querySelectorAll("button");
+let playerWins = 0,
+  computerWins = 0,
+  count = 0;
 function selectedChoice() {
   button.forEach((choice) => {
     choice.addEventListener("click", () => {
-      playRound(choice.textContent.toLowerCase(), computerSelection);
+      count++;
+      game(choice.textContent.toLowerCase());
     });
   });
 }
@@ -11,7 +15,7 @@ function computerChoices() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 const playerSelection = selectedChoice();
-const computerSelection = computerChoices();
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "Rock") {
     console.log("It's a draw");
@@ -21,40 +25,44 @@ function playRound(playerSelection, computerSelection) {
     console.log("It's a draw");
   } else if (playerSelection == "paper" && computerSelection == "Rock") {
     console.log("Player won");
-    // return 1;
+    return 1;
   } else if (playerSelection == "paper" && computerSelection == "Scissor") {
     console.log("Computer Won");
-    // return 0;
+    return 0;
   } else if (playerSelection == "scissor" && computerSelection == "Paper") {
     console.log("Player won");
-    // return 1;
+    return 1;
   } else if (playerSelection == "scissor" && computerSelection == "Rock") {
     console.log("Computer won");
-    // return 0;
+    return 0;
   } else if (playerSelection == "rock" && computerSelection == "Scissor") {
     console.log("Player won");
-    // return 1;
+    return 1;
   } else if (playerSelection == "rock" && computerSelection == "Paper") {
     console.log("Computer won");
-    // return 0;
+    return 0;
   }
 }
-// playRound(playerSelection, computerSelection);
-// function game() {
-// for (i = 0; i < 5; i++) {
-// let val = playRound(playerSelection, computerSelection);
-// if (val == 1) {
-//   playerWins++;
-// } else if (val == 0) {
-//   computerWins++;
-// }
-// }
-// if (playerWins > computerWins) {
-//   console.log("You Win!!!");
-// } else if (computerWins > playerWins) {
-//   console.log("You Lose!!");
-// } else if (computerWins == playerWins) {
-//   console.log("It's a Tie!");
-// }
-// }
-// game();
+const div = document.querySelector("#output");
+div.appendChild(content);
+function game(playerVal) {
+  const computerSelection = computerChoices();
+  let val = playRound(playerVal, computerSelection);
+  if (val == 1) {
+    playerWins++;
+  } else if (val == 0) {
+    computerWins++;
+  }
+  if (count >= 5) {
+    if (playerWins > computerWins) {
+      div.textContent = "You Win!!";
+      console.log("You Win!!!");
+    } else if (computerWins > playerWins) {
+      console.log("You Lose!!");
+      div.textContent = "You Lose!!";
+    } else if (computerWins == playerWins) {
+      console.log("It's a Tie!");
+      div.textContent = "Its a Tie!";
+    }
+  }
+}
